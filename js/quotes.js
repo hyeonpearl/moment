1a08993fdd20008bec4288fc73526df33bd0f981
hyeonpearl <hc9064@gmail.com>
2022-07-23 10:52:40 +0900

    Fix : 한글 명언으로 변경

--- js/quotes.js
+++ js/quotes.js
@@ -1,51 +1,33 @@
 const quotes = [
     {
-        quote: 'You will face many defeats in life, but never let yourself be defeated.',
-        author: 'Maya Angelou',
+        quote: '지나간 슬픔에 새 눈물을 낭비하지 마라',
     },
     {
-        quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
-        author: 'Nelson Mandela',
+        quote: '거짓말은 그 자체가 죄일 뿐 아니라 정신을 더럽힌다',
     },
     {
-        quote: 'Life is either a daring adventure or nothing at all.',
-        author: 'Helen Keller',
+        quote: '그 사람을 알고 싶으면 그의 친구를 보라',
     },
     {
-        quote: 'All you need in this life is ignorance and confidence; then success is sure.',
-        author: 'Mark Twain',
+        quote: '남에게, 또 남의 일에 대해서 말을 삼가라',
     },
     {
-        quote: 'The way to get started is to quit talking and begin doing.',
-        author: 'Walt Disney',
+        quote: '나태라는 어머니는 강도라는 아들과 굶주림이라는 딸을 가지고 있다',
     },
     {
-        quote: `Don't compare yourself with other people, compare yourself with who you were yesterday.`,
-        author: 'Jordan Peterson',
+        quote: `방을 정리하라. 방은 당신의 머릿속의 혼란한 정도를 보여준다.`,
     },
     {
-        quote: `It's in responsibility that most people find the meaning that sustains them through life. It's not in happiness. It's not in impulsive pleasure.`,
-        author: 'Jordan Peterson',
+        quote: `당신에게 최고의 모습을 기대하는 사람만 만나라.`,
     },
     {
-        quote: `'Happiness' is a pointless goal.`,
-        author: 'jordan Peterson',
+        quote: `규율은 자유와 같다.`,
     },
-    {
-        quote: 'Only allow strength and ambition.',
-        author: 'Andrew Tate',
-    },
-    {
-        quote: `Don’t just think. Don’t just talk. Don’t just dream. None of that matters. The only thing that matters is that you actually do. So: DO.`,
-        author: 'Jocko Willink',
-    }
 ]
 
-const quote = document.querySelector('#quote span:first-child');
-const author = document.querySelector('#quote span:last-child');
+const quote = document.querySelector('#quote-line span');
 
 // Math.ceil => 올림   Math.round => 반올림   Math.floor => 버림
 const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todayQuote.quote;
-author.innerText = todayQuote.author;
