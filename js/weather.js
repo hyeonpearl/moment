616e40eaa9c5cf9ad21e313e9069fa3e248b16d5
hyeonpearl <hc9064@gmail.com>
2022-07-31 12:15:37 +0900

    Test : WEATHER API 적용 테스트

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,8 @@
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const WEATHER_API = config.API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API}&units=metric`;
     fetch(url)
     .then(response => response.json())
     .then(data => {
