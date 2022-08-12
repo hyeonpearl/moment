939a1342fb82e3f4be58ea394c07ce743b7424fa
hyeonpearl <hc9064@gmail.com>
2022-08-12 23:45:12 +0900

    Feat : 배경화면 추가 및 CSS 수정

--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,20 @@
 function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const WEATHER_API = config.API_KEY;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API}&units=metric`;
-    fetch(url)
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const WEATHER_API = config.API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API}&units=metric`;
+  fetch(url)
     .then(response => response.json())
     .then(data => {
-        const weather = document.querySelector('#weather span:first-child');
-        const city = document.querySelector('#weather span:last-child');
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-        city.innerText = data.name;
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
     });
 }
 
 function onGeoError() {
-    alert(`위치 정보를 불러올 수 없습니다.`);
+  alert(`위치 정보를 불러올 수 없습니다.`);
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
