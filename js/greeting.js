399617d8f6ceb17e46e1536a0883a2f9f9d5dcb9
hyeonpearl <hc9064@gmail.com>
2022-08-14 15:50:41 +0900

    Feat : 로그아웃 기능 추가, 아이콘 추가

--- js/greeting.js
+++ js/greeting.js
@@ -2,29 +2,37 @@ const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 const mainScreen = document.querySelector('#main-screen');
+const logoutBtn = document.querySelector('#logout-btn');
 
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
-}   
-
-function paintGreetings(username) {
-    greeting.innerText = `${username}님, 반가워요.`;
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    mainScreen.classList.remove(HIDDEN_CLASSNAME);
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+  location.reload();
 }
+function paintGreetings(username) {
+  greeting.innerText = `${username}님, 반가워요.`;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  mainScreen.classList.remove(HIDDEN_CLASSNAME);
+}
+function onLogoutSubmit() {
+  localStorage.removeItem(USERNAME_KEY);
+  location.reload();
+}
+
+logoutBtn.addEventListener('click', onLogoutSubmit);
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener('submit', onLoginSubmit);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  logoutBtn.classList.toggle(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
 } else {
-    paintGreetings(savedUsername);
+  paintGreetings(savedUsername);
 }
