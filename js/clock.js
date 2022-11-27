87e203462cff5fc34e15ab4ac735b36f1ded7478
hyeonpearl <hc9064@gmail.com>
2022-11-28 01:42:14 +0900

    refactor: prettier 적용

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,10 @@
 const clock = document.querySelector('span#clock');
 
 function getClock() {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    clock.innerText = `${hours}:${minutes}`;
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  clock.innerText = `${hours}:${minutes}`;
 }
 
 getClock();
--- js/todo.js
+++ js/todo.js
@@ -4,43 +4,43 @@ const toDoList = document.querySelector('#todo-list');
 
 const TODOS_KEY = 'toDos';
 
-let toDos= [];
+let toDos = [];
 
 function saveToDos() {
-    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
-    const li = event.target.parentElement;
-    li.remove();
-    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
-    saveToDos();
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newToDo) {
-    const li = document.createElement('li');
-    li.id = newToDo.id;
-    const span = document.createElement('span');
-    span.innerText = newToDo.text;
-    const btn = document.createElement('button');
-    btn.innerText = '❌';
-    btn.addEventListener('click', deleteToDo);
-    li.appendChild(span);
-    li.appendChild(btn);
-    toDoList.appendChild(li);
+  const li = document.createElement('li');
+  li.id = newToDo.id;
+  const span = document.createElement('span');
+  span.innerText = newToDo.text;
+  const btn = document.createElement('button');
+  btn.innerText = '❌';
+  btn.addEventListener('click', deleteToDo);
+  li.appendChild(span);
+  li.appendChild(btn);
+  toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
-    event.preventDefault();
-    const newToDo = toDoInput.value;
-    toDoInput.value = "";
-    const newToDoObj = {
-        text : newToDo,
-        id : Date.now(), 
-    };
-    toDos.push(newToDoObj);
-    paintToDo(newToDoObj);
-    saveToDos();
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = '';
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
@@ -48,8 +48,7 @@ toDoForm.addEventListener('submit', handleToDoSubmit);
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
-    const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    parsedToDos.forEach(paintToDo);
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
-
