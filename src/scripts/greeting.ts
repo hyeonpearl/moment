55c6a3941e0fe567d1a53cfd00eda7ec1f5c82b5
hyeonpearl <hc9064@gmail.com>
2023-04-05 12:29:10 +0900

    fix: login form 안 나오는 버그 수정

--- src/scripts/greeting.ts
+++ src/scripts/greeting.ts
@@ -1,19 +1,30 @@
 const loginForm = document.querySelector<HTMLFormElement>('#login-form');
 const loginInput =
   document.querySelector<HTMLInputElement>('#login-form input');
-const greeting = document.querySelector<HTMLSpanElement>('#greeting');
-const mainScreen = document.querySelector<HTMLElement>('#main-screen');
+const greeting = document.querySelector<HTMLHeadingElement>('#greeting');
+const mainScreen = document.querySelector<HTMLDivElement>('#main-screen');
 const logoutBtn = document?.querySelector<HTMLButtonElement>('#logout-btn');
 
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = 'username';
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  if (loginForm !== null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);
+  }
+  if (logoutBtn instanceof HTMLButtonElement) {
+    logoutBtn.classList.toggle(HIDDEN_CLASSNAME);
+  }
+} else {
+  paintGreetings(savedUsername);
+}
+
 function onLoginSubmit(event: Event) {
   event.preventDefault();
-  if (
-    loginForm instanceof HTMLFormElement &&
-    loginInput instanceof HTMLInputElement
-  ) {
+  if (loginForm !== null && loginInput !== null) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
@@ -23,11 +34,7 @@ function onLoginSubmit(event: Event) {
 }
 
 function paintGreetings(username: string) {
-  if (
-    greeting instanceof HTMLSpanElement &&
-    loginForm instanceof HTMLFormElement &&
-    mainScreen instanceof HTMLElement
-  ) {
+  if (greeting !== null && loginForm !== null && mainScreen !== null) {
     greeting.innerText = `${username}님, 반가워요.`;
     loginForm.classList.add(HIDDEN_CLASSNAME);
     mainScreen.classList.remove(HIDDEN_CLASSNAME);
@@ -38,20 +45,7 @@ function onLogoutSubmit() {
   localStorage.removeItem(USERNAME_KEY);
   location.reload();
 }
-if (logoutBtn instanceof HTMLElement) {
+
+if (logoutBtn instanceof HTMLButtonElement) {
   logoutBtn.addEventListener('click', onLogoutSubmit);
-}
-
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-if (savedUsername === null) {
-  if (loginForm instanceof HTMLFormElement) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener('submit', onLoginSubmit);
-  }
-  if (logoutBtn instanceof HTMLButtonElement) {
-    logoutBtn.classList.toggle(HIDDEN_CLASSNAME);
-  }
-} else {
-  paintGreetings(savedUsername);
 }
