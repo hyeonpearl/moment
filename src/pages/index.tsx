b43f17741bad8a3d4854f24b0c8a70fa05656b80
hyeonpearl <hc9064@gmail.com>
2023-04-23 16:29:41 +0900

    CHORE: Setting jest, react-testing-library

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,8 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <header>Header</header>
+
       <main>Todo</main>
-      <footer>Quotes</footer>
     </>
   );
 }
---
+++ src/tests/Home.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+
+import Home from '@pages/index';
+
+describe('<Home />', () => {
+  it('renders a heading', () => {
+    render(<Home />);
+    screen.getByText('Todo');
+  });
+});
